eee45e9fee04eb8d7941942338aa3b8569520c77
maya-honey <sasaki123.321ikasas@gmail.com>
2024-04-30 22:20:21 +0900

    管理者：要素別の表示制限

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,36 @@
-export default function Admin() {
+import { auth } from "@/auth"
+
+export default async function Admin() {
+    const session = await auth()
     return (
         <div>
             <p>Adminのトップページ</p>
             <p>権限のないページにアクセスした場合は、このページにリダイレクトされる</p>
-            <p>ページ一覧</p>
-            <ul>
-                {
-                PathList.map((path, index) => {
-                    return (
-                    <li key={path.path}>
-                        <a className="text-blue-600" href={path.path}>{path.path}</a>：{path.text}
-                    </li>
-                    )
-                })
+            <div className="py-12">
+                <p>ページ一覧</p>
+                <ul>
+                    {
+                        PathList.map((path, index) => {
+                            return (
+                                <li key={path.path}>
+                            <a className="text-blue-600" href={path.path}>{path.path}</a>：{path.text}
+                        </li>
+                        )
+                    })
                 }
-            </ul>
+                </ul>
+            </div>
+            <div className="py-12">
+                <p>権限別の表示制御</p>
+                <ul>
+                    <li>role1：{session?.user?.role === 1 && 'role1だけにこの秘密の文字が見えてる'}</li>
+                    <li>role3：{session?.user?.role === 3 && 'role3だけにこの秘密の文字が見えてる'}</li>
+                    <li>role5：{session?.user?.role === 5 && 'role5だけにこの秘密の文字が見えてる'}</li>
+                    <li>role6：{session?.user?.role === 6 && 'role6だけにこの秘密の文字が見えてる'}</li>
+                    <li>role7：{session?.user?.role === 7 && 'role7だけにこの秘密の文字が見えてる'}</li>
+                    <li>role1+3：{(session?.user?.role === 1 || session?.user?.role === 3) && 'role1と3だけにこの秘密の文字が見えてる'}</li>
+                </ul>
+            </div>
         </div>
     )
 }
