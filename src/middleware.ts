ecfc4d6cb77f74948291cf1cf9c026070bfdc4e4
maya-honey <sasaki123.321ikasas@gmail.com>
2024-04-30 20:23:37 +0900

    マイページの未ログインユーザー制限

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,11 @@
-export { auth as middleware } from "@/auth"
-/**
- * import { auth } from "@/auth"
+import { auth } from "@/auth"
 
 export default auth((req) => {
-    console.log('これだべべ', req.auth?.user)
     if (!req.auth) {
-        const url = req.url.replace(req.nextUrl.pathname, '/login')
+        const url = req.url.replace(req.nextUrl.pathname, '/signin')
         return Response.redirect(url)
     }
 })
-**/
 
 export const config = {
     matcher: ['/mypage'],
